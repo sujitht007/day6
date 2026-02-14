e922f9a132b705008ceaf535014f4af17b6d69bc
sujitht007 <155134448+sujitht007@users.noreply.github.com>
2026-02-14 10:51:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "aws_region" {
-  type    = string
-  default = "eu-north-1"
+  description = "AWS Region"
+  type        = string
+  default     = "eu-north-1"
 }
-variable "instance_type" {
-  type    = string
-  default = "t3.micro"
-}
-variable "ami_id" {
-  type = string
 
-  validation {
-    condition     = length(trimspace(var.ami_id)) > 0
-    error_message = "ami-073130f74f5ffb161"
-  }
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t3.micro"
 }
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+  default     = "ami-073130f74f5ffb161"
+}
+
 variable "existing_key_pair_name" {
-  type    = string
-  default = "devops"
+  description = "Existing AWS Key Pair Name"
+  type        = string
+  default     = "devops"
 }
+
 variable "allowed_ssh_cidr" {
-  type    = string
-  default = "0.0.0.0/0"
+  description = "CIDR allowed for SSH"
+  type        = string
+  default     = "0.0.0.0/0"
 }
