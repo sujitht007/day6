42cda818d52c1376cfa6e208b67c8d18f2ad33a7
sujitht007 <155134448+sujitht007@users.noreply.github.com>
2026-02-14 11:08:08 +0530

    Modify AWS region and validate ami_id variable
    Updated default AWS region and added validation for ami_id.

--- variables.tf
+++ variables.tf
@@ -1,29 +1,24 @@
 variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "eu-north-1"
+  type    = string
+  default = "us-east-1"
 }
-
 variable "instance_type" {
-  description = "EC2 Instance Type"
-  type        = string
-  default     = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
-
 variable "ami_id" {
-  description = "AMI ID for EC2"
-  type        = string
-  default     = "ami-073130f74f5ffb161"
-}
+  type = string
 
+  validation {
+    condition     = length(trimspace(var.ami_id)) > 0
+    error_message = "ami-0b6c6ebed2801a5cb"
+  }
+}
 variable "existing_key_pair_name" {
-  description = "Existing AWS Key Pair Name"
-  type        = string
-  default     = "devops"
+  type    = string
+  default = "devops"
 }
-
 variable "allowed_ssh_cidr" {
-  description = "CIDR allowed for SSH"
-  type        = string
-  default     = "0.0.0.0/0"
+  type    = string
+  default = "0.0.0.0/0"
 }
