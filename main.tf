4b84d2a2322c772424ce01c73e1d28c9e46b6031
sujitht007 <155134448+sujitht007@users.noreply.github.com>
2026-02-14 11:07:02 +0530

    Refactor AWS resources in main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,30 @@
-resource "aws_security_group" "ssh_sg" {
-  name        = "terraform-jenkins-sg"
-  description = "Allow SSH access"
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+data "aws_vpc" "default" {
+  default = true
+}
+data "aws_subnets" "default_in_vpc" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+resource "aws_security_group" "ec2_sg" {
+  name        = "terraform-ec2-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
   ingress {
     description = "SSH"
     from_port   = 22
@@ -9,22 +32,31 @@ resource "aws_security_group" "ssh_sg" {
     protocol    = "tcp"
     cidr_blocks = [var.allowed_ssh_cidr]
   }
-
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_instance" "jenkins_instance" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.existing_key_pair_name
-  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
-
   tags = {
-    Name = "Terraform-Jenkins-Instance"
+    Name = "terraform-ec2-sg"
+  }
+}
+resource "aws_instance" "ec2" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = data.aws_subnets.default_in_vpc.ids[0]
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+  key_name = var.existing_key_pair_name
+  tags = {
+    Name = "terraform-ec2"
   }
 }
