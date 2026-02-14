c23890bfdae758c508280f08d8c926fdde413d7c
sujitht007 <155134448+sujitht007@users.noreply.github.com>
2026-02-14 11:07:40 +0530

    Refactor outputs to use EC2 instance and key pair
    Updated outputs to reference new EC2 instance and added key pair output.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_id" {
-  value = aws_instance.jenkins_instance.id
+  value = aws_instance.ec2.id
 }
 
 output "public_ip" {
-  value = aws_instance.jenkins_instance.public_ip
+  value = aws_instance.ec2.public_ip
 }
 
-output "public_dns" {
-  value = aws_instance.jenkins_instance.public_dns
+output "key_pair_used" {
+  value = var.existing_key_pair_name
 }
